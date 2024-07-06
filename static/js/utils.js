df84f6e8a4f01727c38fb91ba2c364ff8a20f0e0
Stuart Wood <87521783+stuartw843@users.noreply.github.com>
2024-07-06 18:36:41 +0100

    Update utils.js

--- static/js/utils.js
+++ static/js/utils.js
@@ -21,3 +21,22 @@ function updateSyncStatus(status) {
     syncStatus.classList.remove('online', 'offline', 'syncing');
     syncStatus.classList.add(status);
 }
+
+window.addEventListener('popstate', function(event) {
+    if (event.state) {
+        showPage(event.state.page);
+    }
+});
+
+function showPage(page) {
+    history.pushState({ page: page }, '', `#${page}`);
+    document.getElementById('notes-page').classList.add('hidden');
+    document.getElementById('tasks-page').classList.add('hidden');
+    document.getElementById(page + '-page').classList.remove('hidden');
+    if (page === 'notes') {
+        displayNotes();
+    } else if (page === 'tasks') {
+        displayTasks();
+    }
+}
+
