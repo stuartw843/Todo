ea5f92a734ef56d6b3e66915b2446601d5e3a9a8
Stuart Wood <87521783+stuartw843@users.noreply.github.com>
2024-07-08 09:48:20 +0100

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -495,7 +495,7 @@ window.addEventListener('online', () => {
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js').then(registration => {
+        navigator.serviceWorker.register('static/service-worker.js').then(registration => {
             console.log('ServiceWorker registration successful with scope: ', registration.scope);
         }, err => {
             console.log('ServiceWorker registration failed: ', err);
