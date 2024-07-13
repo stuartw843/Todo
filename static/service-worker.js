3885d4c2185f2417cbc47ff55bfa26786b70ddd2
Stuart Wood <87521783+stuartw843@users.noreply.github.com>
2024-07-13 16:26:02 +0100

    Update service-worker.js

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,17 +1,16 @@
-const CACHE_NAME = 'notes-tasks-app-cache-v4';
+const CACHE_NAME = 'notes-tasks-app-cache-v5';
 const urlsToCache = [
-    '/',
-    'index.html',
-    'static/css/style.css',
-    'static/js/main.js',
+    '/Todo/',
+    '/Todo/index.html',
+    '/Todo/static/css/style.css',
+    '/Todo/static/js/main.js',
     'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css',
-    'https://cdn.quilljs.com/1.3.6/quill.snow.css',
-    'https://cdn.quilljs.com/1.3.6/quill.min.js',
-    'https://cdn.jsdelivr.net/npm/pouchdb@7.2.2/dist/pouchdb.min.js',
-    'https://cdn.jsdelivr.net/npm/pouchdb-authentication@1.1.3/dist/pouchdb.authentication.min.js',
+    'https://cdn.jsdelivr.net/npm/pouchdb@9.0.0/dist/pouchdb.min.js',
     'https://cdn.jsdelivr.net/npm/showdown/dist/showdown.min.js',
     'https://cdnjs.cloudflare.com/ajax/libs/fuse.js/6.4.6/fuse.min.js',
-    'https://cdn.jsdelivr.net/npm/uuid/dist/umd/uuid.min.js'
+    'https://cdn.jsdelivr.net/npm/uuid/dist/umd/uuid.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/Sortable/1.15.2/Sortable.min.js',
+    'https://cdn.tiny.cloud/1/subygrexh3q7llzh0xwx7scdfv4hc8zmjp1uejxnadqggr65/tinymce/6/tinymce.min.js' // Update with the correct TinyMCE URL
 ];
 
 self.addEventListener('install', function(event) {
@@ -31,7 +30,25 @@ self.addEventListener('fetch', function(event) {
                 if (response) {
                     return response;
                 }
-                return fetch(event.request);
+                return fetch(event.request).then(function(response) {
+                    // Check if we received a valid response
+                    if(!response || response.status !== 200 || response.type !== 'basic') {
+                        return response;
+                    }
+
+                    // IMPORTANT: Clone the response. A response is a stream
+                    // and because we want the browser to consume the response
+                    // as well as the cache consuming the response, we need
+                    // to clone it so we have two streams.
+                    var responseToCache = response.clone();
+
+                    caches.open(CACHE_NAME)
+                        .then(function(cache) {
+                            cache.put(event.request, responseToCache);
+                        });
+
+                    return response;
+                });
             })
     );
 });
